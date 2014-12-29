1e4353cae5df7fc7d112ceb593986091835d66a5
Mike Causer <mcauser@gmail.com>
2014-12-30 02:19:07 +1100

    Rename to TM1637 and add begin()

--- TM1637.h
+++ TM1637.h
@@ -14,8 +14,8 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef __TM1637DISPLAY__
-#define __TM1637DISPLAY__
+#ifndef __TM1637__
+#define __TM1637__
 
 #include <inttypes.h>
 
@@ -27,24 +27,23 @@
 #define SEG_F   0b00100000
 #define SEG_G   0b01000000
 
-class TM1637Display {
+class TM1637 {
 
 public:
-  //! Initialize a TM1637Display object, setting the clock and
-  //! data pins.
+  //! Initialize a TM1637 object, setting the clock and data pins.
   //!
   //! @param pinClk - The number of the digital pin connected to the clock pin of the module
   //! @param pinDIO - The number of the digital pin connected to the DIO pin of the module
-  TM1637Display(uint8_t pinClk, uint8_t pinDIO);
-  
+  //TM1637(uint8_t pinClk, uint8_t pinDIO);
+  void begin(uint8_t pinClk, uint8_t pinDIO);
+
   //! Sets the brightness of the display.
   //!
-  //! The setting takes effect when a command is given to change the data being
-  //! displayed.
+  //! The setting takes effect when a command is given to change the data being displayed.
   //!
-  //! @param brightness A number from 0 (lowes brightness) to 7 (highest brightness)
+  //! @param brightness A number from 0 (lowest brightness) to 7 (highest brightness)
   void setBrightness(uint8_t brightness);
-  
+
   //! Display arbitrary data on the module
   //!
   //! This function receives raw segment values as input and displays them. The segment data
@@ -58,20 +57,20 @@ public:
   //! @param length The number of digits to be modified
   //! @param pos The position from which to start the modification (0 - leftmost, 3 - rightmost)
   void setSegments(const uint8_t segments[], uint8_t length = 4, uint8_t pos = 0);
-  
-  //! Displayes a decimal number
+
+  //! Displays a decimal number
   //!
-  //! Dispalyes the given argument as a decimal number
+  //! Displays the given argument as a decimal number
   //!
   //! @param num The number to be shown
-  //! @param leading_zero When true, leading zeros are displayed. Otherwise unnecessary digits are
-  //!        blank
+  //! @param leading_zero When true, leading zeros are displayed. Otherwise unnecessary digits are blank
   //! @param length The number of digits to set. The user must ensure that the number to be shown
   //!        fits to the number of digits requested (for example, if two digits are to be displayed,
   //!        the number must be between 0 to 99)
   //! @param pos The position least significant digit (0 - leftmost, 3 - rightmost)
-  void showNumberDec(int num, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
-  
+  //! @param colon When true, displays two dots between the 2nd and 3rd segments
+  void showNumberDec(int num, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0, bool colon = false);
+
   //! Translate a single digit into 7 segment code
   //!
   //! The method accepts a number between 0 - 15 and converts it to the
@@ -84,18 +83,15 @@ public:
   uint8_t encodeDigit(uint8_t digit);
 
 protected:
-   void bitDelay();
-   
-   void start();
-   
-   void stop();
-   
-   bool writeByte(uint8_t b);
-   
+  void bitDelay();
+  void start();
+  void stop();
+  bool writeByte(uint8_t b);
+
 private:
-	uint8_t m_pinClk;
-	uint8_t m_pinDIO;
-	uint8_t m_brightness;
+  uint8_t m_pinClk;
+  uint8_t m_pinDIO;
+  uint8_t m_brightness;
 };
 
-#endif // __TM1637DISPLAY__
+#endif // __TM1637__
